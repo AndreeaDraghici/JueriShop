4197c2f841acf52696b7aa59e9a892e08b61d626
Drăghici Andreea-Maria <draghici108@gmail.com>
2022-12-18 12:26:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using JueriOnlineShop.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//builder.Services.AddDbContext<DatabaseContext>(dbContextOptions => dbContextOptions.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<DatabaseContext>();
+
+builder.Services.AddDbContext<DatabaseContext>(dbContextOptions => dbContextOptions.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# JueriShop
-ECommerce Project
+﻿## Readme 
 
-Team Members: Draghici Andreea-Maria & Ciontu Claudia-Elena
+## NOTES:
 
-Project Title: Magazin online de bijuterii
+I divided the application into several features to make it easier to implement the requirements, at the end we will make it go to the development branch.
+
+## Technical aspects:
+
+- **IDE:** Visual Studio Community 2022
+
+- **Language:** C#
+
+- **Framework:** DotNet , Entity Framework Core
+
+## History:
+
+- Initial, I created the skelet code , next steps was to implement the MVC pattern and create the database connection (dbcontext, migration).
+
